7bd7791a445a672ad057134e6fd609c1fa53328a
mark-steven <mark@outbackagency.com.au>
2020-11-04 21:16:56 +1100

    withauth2

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -3,21 +3,22 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
+const [user, setUser] = useState(null);
+const [admin, setAdmin] = useState(false);
+const logger = new Logger("My-Logger");
 
+const [user, setUser] = useState(null);
+const [admin, setAdmin] = useState(false);
+const logger = new Logger("My-Logger");
 
 const Projects = () => (
-    <>
-    
-            <Layout>
-              
-                <h1>Projects Public Page</h1>
-                
-                <Link to="/">Go back  homepage</Link>
-             
-              
-              </Layout>
-     
-        </>
-)
+  <>
+    <Layout>
+      <h1>Projects Public Page</h1>
+
+      <Link to="/">Go back homepage</Link>
+    </Layout>
+  </>
+);
 
 export default Projects;
